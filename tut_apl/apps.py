b7185f64361936ed271455aacad42262b5a9e433
Arjun Shaji <arjunshaji54@gmail.com>
2023-01-31 22:51:41 +0530

    commited1

---
+++ tut_apl/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TutAplConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tut_apl'
---
+++ tut_apl/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+
+
+# Overriding the Default Django Auth User and adding One More Field (user_type)
+class CustomUser(AbstractUser):
+    user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
