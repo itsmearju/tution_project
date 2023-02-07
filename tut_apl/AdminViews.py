90dde9dddaee0a557091645d5877a15d34010db4
Arjun Shaji <arjunshaji54@gmail.com>
2023-02-07 23:39:27 +0530

    commited7

--- tut_apl/AdminViews.py
+++ tut_apl/AdminViews.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from tut_apl.models import CustomUser, Staffs, Students
-from .forms import AddStudentForm
+from .forms import AddStudentForm, EditStudentForm
 from django.core.files.storage import FileSystemStorage #To upload Profile Picture
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 
 def admin_home(request):
     return render(request, "admin_template/home_content.html")
+
+
+def manage_staff(request):
+    staffs = Staffs.objects.all()
+    context = {
+        "staffs": staffs
+    }
+    return render(request, "admin_template/manage_staff_template.html", context)
 
 def add_staff(request):
     return render(request, "admin_template/add_staff_template.html")
@@ -34,6 +43,58 @@ def add_staff_save(request):
         except:
             messages.error(request, "Failed to Add Staff!")
             return redirect('add_staff')
+
+def edit_staff(request, staff_id):
+    staff = Staffs.objects.get(admin=staff_id)
+
+    context = {
+        "staff": staff,
+        "id": staff_id
+    }
+    return render(request, "admin_template/edit_staff_template.html", context)
+
+
+def edit_staff_save(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        staff_id = request.POST.get('staff_id')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+
+        try:
+            # INSERTING into Customuser Model
+            user = CustomUser.objects.get(id=staff_id)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.username = username
+            user.save()
+            
+            # INSERTING into Staff Model
+            staff_model = Staffs.objects.get(admin=staff_id)
+            staff_model.address = address
+            staff_model.save()
+
+            messages.success(request, "Staff Updated Successfully.")
+            return redirect('/edit_staff/'+staff_id)
+
+        except:
+            messages.error(request, "Failed to Update Staff.")
+            return redirect('/edit_staff/'+staff_id)
+
+def delete_staff(request, staff_id):
+    staff = CustomUser.objects.all(user_type=2)
+    try:
+        staff.delete()
+        messages.success(request, "Staff Deleted Successfully.")
+        return redirect('manage_staff')
+    except:
+        messages.error(request, "Failed to Delete Staff.")
+        return redirect('manage_staff')
 
 def add_student(request):
     form = AddStudentForm()
@@ -83,3 +144,101 @@ def add_student_save(request):
                 return redirect('add_student')
         else:
             return redirect('add_student')
+
+def manage_student(request):
+    students = Students.objects.all()
+    context = {
+        "students": students
+    }
+    return render(request, 'admin_template/manage_student_template.html', context)
+
+
+def edit_student(request, student_id):
+    # Adding Student ID into Session Variable
+    request.session['student_id'] = student_id
+
+    student = Students.objects.get(admin=student_id)
+    form = EditStudentForm()
+    # Filling the form with Data from Database
+    form.fields['email'].initial = student.admin.email
+    form.fields['username'].initial = student.admin.username
+    form.fields['first_name'].initial = student.admin.first_name
+    form.fields['last_name'].initial = student.admin.last_name
+    form.fields['address'].initial = student.address
+    form.fields['gender'].initial = student.gender
+
+    context = {
+        "id": student_id,
+        "username": student.admin.username,
+        "form": form
+    }
+    return render(request, "admin_template/edit_student_template.html", context)
+
+
+def edit_student_save(request):
+    if request.method != "POST":
+        return HttpResponse("Invalid Method!")
+    else:
+        student_id = request.session.get('student_id')
+        if student_id == None:
+            return redirect('/manage_student')
+
+        form = EditStudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            address = form.cleaned_data['address']
+            gender = form.cleaned_data['gender']
+
+            # Getting Profile Pic first
+            # First Check whether the file is selected or not
+            # Upload only if file is selected
+            if len(request.FILES) != 0:
+                profile_pic = request.FILES['profile_pic']
+                fs = FileSystemStorage()
+                filename = fs.save(profile_pic.name, profile_pic)
+                profile_pic_url = fs.url(filename)
+            else:
+                profile_pic_url = None
+
+            try:
+                # First Update into Custom User Model
+                user = CustomUser.objects.get(id=student_id)
+                user.first_name = first_name
+                user.last_name = last_name
+                user.email = email
+                user.username = username
+                user.save()
+
+                # Then Update Students Table
+                student_model = Students.objects.get(admin=student_id)
+                student_model.address = address
+
+                student_model.gender = gender
+                if profile_pic_url != None:
+                    student_model.profile_pic = profile_pic_url
+                student_model.save()
+                # Delete student_id SESSION after the data is updated
+                del request.session['student_id']
+
+                messages.success(request, "Student Updated Successfully!")
+                return redirect('/edit_student/'+student_id)
+            except:
+                messages.success(request, "Failed to Uupdate Student.")
+                return redirect('/edit_student/'+student_id)
+        else:
+            return redirect('/edit_student/'+student_id)
+
+
+def delete_student(request, student_id):
+    student = Students.objects.get(admin=student_id)
+    try:
+        student.delete()
+        messages.success(request, "Student Deleted Successfully.")
+        return redirect('manage_student')
+    except:
+        messages.error(request, "Failed to Delete Student.")
+        return redirect('manage_student')
+
--- tut_apl/urls.py
+++ tut_apl/urls.py
@@ -7,11 +7,22 @@ urlpatterns = [
 path('', views.loginPage, name="login"),
 path('doLogin',views.doLogin, name="doLogin"),
 path('logout_user',views.logout_user, name="logout_user"),
+
 path('admin_home',AdminViews.admin_home, name="admin_home"),
+path('manage_staff',AdminViews.manage_staff, name="manage_staff"),
 path('add_staff',AdminViews.add_staff, name="add_staff"),
 path('add_staff_save',AdminViews.add_staff_save, name="add_staff_save"),
+path('edit_staff/<staff_id>/',AdminViews.edit_staff, name="edit_staff"),
+path('edit_staff_save',AdminViews.edit_staff_save, name="edit_staff_save"),
+path('delete_staff/<staff_id>/',AdminViews.delete_staff, name="delete_staff"),
 path('add_student',AdminViews.add_student, name="add_student"),
 path('add_student_save',AdminViews.add_student_save, name="add_student_save"),
+path('manage_student',AdminViews.manage_student, name="manage_student"),
+path('edit_student/<student_id>',AdminViews.edit_student, name="edit_student"),
+path('edit_student_save/',AdminViews.edit_student_save, name="edit_student_save"),
+path('delete_student/<student_id>/',AdminViews.delete_student, name="delete_student"),
+
 path('staff_home',StaffViews.staff_home, name="staff_home"),
+
 path('student_home',StudentViews.student_home, name="student_home"),
 ]
