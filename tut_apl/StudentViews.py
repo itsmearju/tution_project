a8dd0a4c6490b985dc6f68ff8bd39277b567f6b7
Arjun Shaji <arjunshaji54@gmail.com>
2023-02-21 23:36:50 +0530

    profile

--- tut_apl/StudentViews.py
+++ tut_apl/StudentViews.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render, redirect
-from tut_apl.models import CustomUser, Students, AttendanceReport, Attendance, Subjects, StudentResult, LeaveReportStudent, StudentNote, FeedBackStudent
+from tut_apl.models import CustomUser, Students, AttendanceReport, Attendance, Subjects, StudentResult, LeaveReportStudent, StudentNote, FeedBackStudent, Courses
 from django.contrib import messages
 import datetime # To Parse input DateTime into Python Date Time Object
 
 
 def student_home(request):
-    return render(request, "student_template/student_home_template.html")
+    student_obj = Students.objects.get(admin=request.user.id)
+    total_attendance = AttendanceReport.objects.filter(student_id=student_obj).count()
+    attendance_present = AttendanceReport.objects.filter(student_id=student_obj, status=True).count()
+    attendance_absent = AttendanceReport.objects.filter(student_id=student_obj, status=False).count()
+
+    course_obj = Courses.objects.get(id=student_obj.course_id.id)
+    total_subjects = Subjects.objects.filter(course_id=course_obj).count()
+
+    subject_name = []
+    data_present = []
+    data_absent = []
+    subject_data = Subjects.objects.filter(course_id=student_obj.course_id)
+    for subject in subject_data:
+        attendance = Attendance.objects.filter(subject_id=subject.id)
+        attendance_present_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=True, student_id=student_obj.id).count()
+        attendance_absent_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=False, student_id=student_obj.id).count()
+        subject_name.append(subject.subject_name)
+        data_present.append(attendance_present_count)
+        data_absent.append(attendance_absent_count)
+
+    student = Students.objects.get(admin=request.user.id)
+
+    context={
+        "total_attendance": total_attendance,
+        "attendance_present": attendance_present,
+        "attendance_absent": attendance_absent,
+        "course_obj":course_obj,
+        "total_subjects": total_subjects,
+        "subject_name": subject_name,
+        "data_present": data_present,
+        "data_absent": data_absent,
+        "student":student
+    }
+    return render(request, "student_template/student_home_template.html", context)
 
 
 def student_profile(request):
